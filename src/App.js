f55682486af111e9f0b98296adb8b3882c928e14
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-04-28 18:55:30 +0900

    feat: useEffect 추가, React18버전으로 문법 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import Button from "./Button";
-import styles from "./App.module.css"
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("2");
+  }, [keyword, counter]);
   return (
     <div>
-      <h1
-        className={styles.title}
+      <input 
+        value={keyword} 
+        onChange={onChange} 
+        type="text" 
+        placeholder="Search here..." 
+      />
+      <h1>{counter}</h1>
+      <button
+        onClick={onClick}
         >
-          Welcome back!
-      </h1>
-      <Button text={"Continue"} />
+        click me
+      </button>
     </div>
   );
 }
