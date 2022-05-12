98d68abef7b92b0a8f261fdff2b011e28fdc3cc5
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-05-12 17:25:28 +0900

    fix: 폴더 수정

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
-import Practice01 from './practice01/Practice01';
+import Practice01 from './practice/practice01/Practice01';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
