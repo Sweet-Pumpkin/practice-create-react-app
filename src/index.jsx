a66cfdaaa95f3a7e6ab2f0e40038b52d07633f78
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-05-18 14:13:17 +0900

    feat: msw 학습

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import StyledComponents from './practice/220517/StyledComponents';
+import Mocking from './practice/220518/Mocking';
+
+// Start the mocking conditionally.
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser')
+  worker.start()
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StyledComponents />
+  <Mocking />
 )
