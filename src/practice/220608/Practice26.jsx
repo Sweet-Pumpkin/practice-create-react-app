5dc6f242a0b5ce93e3502792064ee5e920110d6e
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-06-10 20:47:48 +0900

    feat: Megabyte School React 강의 20220610-01

--- src/practice/220608/Practice26.jsx
+++ src/practice/220608/Practice26.jsx
@@ -1,14 +1,6 @@
-import Link from './Link';
-
-const RandomEmail = (props) => {
-  return (
-    <div>
-      Email: {props.someData}!
-      <br />
-      <Link href='/'>reset</Link>
-    </div>
-  )
-}
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 const exampleApi = (gender) => fetch(`https://randomuser.me/api/?gender=${gender}`, {
   headers: {
@@ -22,15 +14,78 @@ const exampleApi = (gender) => fetch(`https://randomuser.me/api/?gender=${gender
   return `${randomUser.gender} - ${randomUser.name.first}`;
 });
 
-// Server Side에서 사용자 요청마다 실행될 javascript
-export const getServerSideProps = async (props) => {
-  const gender = props.query.gender;
-  if (gender === 'male' || gender === 'female') {
-    const someData = await exampleApi(gender);
-    return { props: { someData } };
-  } else {
-    return { props: { someData: '' } };
-  }
+const registerThunk = (gender) => async (dispatch, getState) => {
+  dispatch({ type: 'LOGIN_REQUEST' });
+  const randomName = await exampleApi(gender);
+  dispatch({ type: 'LOGIN', payload: randomName });
 }
 
-export default RandomEmail;
+const App = () => {
+  const id = useSelector((state) => state.user.name);
+
+  return (
+    <>
+      {id ? (
+        <Routes>
+          <Route path="/" element={<Hello />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/register" element={<Form />} />
+          <Route path="*" element={<Navigate to="/register" replace />} />
+        </Routes>
+      )}
+    </>
+  );
+};
+
+const Form = () => {
+  const isFetching = useSelector((state) => state.user.isFetching);
+  const [inputValue, setInputValue] = React.useState("");
+  const dispatch = useDispatch();
+  const handleChangeId = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleClick = () => {
+    const returnedActionFunction = registerThunk(inputValue);
+    dispatch(returnedActionFunction);
+  };
+
+  return (
+    <div>
+      <input
+        data-testid="id-input"
+        type="text"
+        name="id"
+        placeholder='gender'
+        value={inputValue}
+        onChange={handleChangeId}
+      />
+      <button type="button" onClick={handleClick}>
+        회원가입
+      </button>
+      {isFetching && '진행중'}
+    </div>
+  );
+};
+
+const Hello = () => {
+  const id = useSelector((state) => state.user.name);
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
+
+  return (
+    <>
+      <div>안녕하세요 {id}님!</div>
+      <button type="button" onClick={handleLogout}>
+        로그아웃
+      </button>
+    </>
+  );
+};
+
+export default App;
--- src/practice/220608/store.js
+++ src/practice/220608/store.js
@@ -1,13 +1,45 @@
-import { useReducer } from "react";
 import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunkMiddleware from "redux-thunk";
 
+const INITIAL_STATE = {
+  isFetching: false,
+  name: '',
+};
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "LOGIN_REQUEST": {
+      return {
+        ...state,
+        isFetching: true,
+      } 
+    } 
+    case "LOGIN":
+      return {
+        isFetching: false,
+        name: action.payload,
+      } 
+    case "LOGOUT":
+      return { ...INITIAL_STATE };
+    default: 
+      return state;
+  }   
+};
+
+function thunkMiddleware(store) {
+  return function (next) {
+    return function (action) {
+      if (typeof action === 'function') {
+        action(store.dispatch, store.getState);
+      } else {
+        next(action);
+      } 
+    };
+  };
+};
 
 const store = createStore(
   combineReducers({
-    user: useReducer,
+    user: userReducer,
   }),
-  applyMiddleware(thunkMiddleware)
+  applyMiddleware(thunkMiddleware),
 );
-
 export default store;
